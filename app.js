022a2730bdaa2a4828009d9ea9da7a73a422ed8f
Sarah Morris <sarah.morris9@gmail.com>
2018-04-17 11:57:42 -0500

    Might have completed exercise?

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res, next) => {
+  res.send('Hello World - go to localhost:8080/index.html to see a cat?')
+})
+
 app.use(express.static('public'))
 
-app.listen(3000)
+
+app.use( (err, req, res, next) => {
+  // all errors go to here
+  res.status(err.status || 500)
+  res.json({
+      message: "Error error error!",
+      err: err.message
+  });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
